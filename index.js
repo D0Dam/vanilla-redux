d89f104963251501f1d692ca0bab99010b53a0a3
KimMinJae <smallkdb@gmail.com>
2022-04-28 00:06:31 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import { createStore } from "redux";
+const initialState = {
+	toggle: false,
+	counter: 0,
+};
+const store = createStore(reducer);
+const divToggle = document.querySelector(".toggle");
+const counteras = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+function reducer(state = initialState, action) {
+	console.log(state);
+	switch (action.type) {
+		case TOGGLE_SWITCH:
+			return {
+				...state,
+				toggle: !state.toggle,
+			};
+		case INCREASE:
+			return {
+				...state,
+				counter: state.counter + action.difference,
+			};
+		case DECREASE:
+			return {
+				...state,
+				counter: state.counter - 1,
+			};
+		default:
+			return state;
+	}
+}
+
+divToggle.onclick = () => {
+	store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+	store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+	store.dispatch(decrease());
+};
+
+const render = () => {
+	const state = store.getState();
+	if (state.toggle) {
+		divToggle.classList.add("active");
+	} else {
+		divToggle.classList.remove("active");
+	}
+	console.log(state.counter);
+	counteras.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
